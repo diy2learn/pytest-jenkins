f96a73fd185d4011131c703f7703da654fd7971e
diy2learn <bian.tquang@gmail.com>
2020-12-20 11:27:20 +0100

    added a dummy test

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -7,6 +7,11 @@ def test_add():
     assert result == 8
 
 
+def test_add_zero():
+    result = add(3, 0)
+    assert result == 3
+
+
 def test_add_string():
     with pytest.raises(TypeError):
         add("string", 4)
